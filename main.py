0ad0b8a99809e5d38f4a04c42c7224290267fd1c
Abdulaziz2002Umurzoqov <143340706+Abdulaziz2002Umurzoqov@users.noreply.github.com>
2023-12-19 13:15:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-class Employee:
+import re
 
-    company = 'ABC'
+namuna = re.compile("[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+")
+class UserProfile:
 
-    def __init__(self, name, age):
-        self.name = name
+    def __init__(self, username, email, age):
+        self.username = username
+        self.email = email
         self.age = age
 
-    @property
-    def name(self):
-        return self._name
 
-    @name.setter
-    def name(self, value):
-        if len(value) > 15:
-            raise ValueError('Name should be less than 15 chars')
-        self._name = value
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        if not namuna.fullmatch(value):
+            raise ValueError("Error email address.")
+        self._email = value
+
 
     @property
     def age(self):
         return self._age
 
     @age.setter
-    def age(self, qiymat):
-        if qiymat < 0 or qiymat > 18:
-            raise ValueError("Age should be up than 18")
-        self._age = qiymat
+    def age(self, value):
+        if value < 0:
+            raise ValueError("The age is incorrect.Make sure that is available")
+        self._age = value
 
-# attribute
-# method
-# property
 
-emp1 = Employee('Salom', 22)
-print(emp1.name, emp1.age)
+
+user1 = UserProfile('Anvarbek', 'anvar@gmail.com', 25)
+print(user1.email)
+print(user1.age)
